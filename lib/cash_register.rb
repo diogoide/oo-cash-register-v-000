58ecc70577de2911e419ece2a4a21bcefcd75974
Diogo Ide <diogoide@gmail.com>
2018-04-22 19:02:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+
+  def add_item(title, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @cart << item_info 
+  end
+
+
+end
