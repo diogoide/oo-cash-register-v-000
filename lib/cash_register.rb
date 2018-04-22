cd2b7daf6afaf9455058decd99251fe65f10c27b
Diogo Ide <diogoide@gmail.com>
2018-04-22 19:13:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,11 @@ class CashRegister
   end
 
   def apply_discount
-    @total * @discount 
+    if discount > 0
+      @total = @total * @discount
+      puts "After the discount, the total comes to #{total}."
+    else
+      @total
   end
 
 
