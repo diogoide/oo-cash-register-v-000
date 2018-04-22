b8e02ce75ea5a7ff18ef1fdc7c1c078acc0fee26
Diogo Ide <diogoide@gmail.com>
2018-04-22 19:46:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,7 +33,7 @@ class CashRegister
     item_names = []
     @cart.each do |item_info|
       for qty in 1..item_info[:quantity]
-        item_names << item_info[:name]
+        item_names << item_info[:item]
       end
     end
     item_names
