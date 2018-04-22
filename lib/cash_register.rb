c7eb9ee2c437844f0f7a442e84f1fc97ac7c62fd
Diogo Ide <diogoide@gmail.com>
2018-04-22 19:18:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   end
 
   def apply_discount
-    if discount != 0
+    if @discount != 0
       @total -= @total * @discount /100
     else
       "There is no discount to apply"
